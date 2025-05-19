71bc23f59448637d0b2fe5070a7b930845d4a74c
sseobi16 <jseobi3951@naver.com>
2025-05-20 03:31:39 +0900

    feat: UserRepository 메소드 정의

--- src/main/java/com/DionysOS/Eatmoji/repository/UserRepository.java
+++ src/main/java/com/DionysOS/Eatmoji/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.DionysOS.Eatmoji.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     boolean existsByEmail(String email);  // 이메일 중복 체크용
+
+    Optional<User> findByEmail(String email); // 로그인 시에 사용할 이메일 조회
 }

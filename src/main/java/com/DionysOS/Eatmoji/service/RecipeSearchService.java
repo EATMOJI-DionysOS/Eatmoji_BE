2db6801f74b16e9dc3d04983c0d0909f755f304d
sseobi16 <jseobi3951@naver.com>
2025-06-04 17:14:13 +0900

    faet: 배포 시에 .env 파일 삭제 및 .yml 에서 설정에 따른 수정 #19

--- src/main/java/com/DionysOS/Eatmoji/service/RecipeSearchService.java
+++ src/main/java/com/DionysOS/Eatmoji/service/RecipeSearchService.java
@@ -3,8 +3,8 @@ package com.DionysOS.Eatmoji.service;
 import com.DionysOS.Eatmoji.util.CsvUtil;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.S3Object;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.InputStream;
 
@@ -12,14 +12,15 @@ import java.io.InputStream;
 public class RecipeSearchService {
 
     private final AmazonS3 s3Client;
-    private final String bucketName;
-    private final String fileName;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${cloud.aws.s3.filename}")
+    private String fileName;
 
     public RecipeSearchService(AmazonS3 s3Client) {
         this.s3Client = s3Client;
-        Dotenv dotenv = Dotenv.load();
-        this.bucketName = dotenv.get("S3_BUCKET_NAME");
-        this.fileName = dotenv.get("FILE_NAME");
     }
 
     // 레시피가 없을 때의 예외 처리 필요
@@ -28,11 +29,15 @@ public class RecipeSearchService {
             String recipe;
             S3Object s3Object = s3Client.getObject(bucketName, fileName);
             InputStream inputStream = s3Object.getObjectContent();
-            recipe =  CsvUtil.findColumnValue(inputStream, 2, food, 0);  //  RCP_SNO: 1열, CKG_NM: 3열
+
+            //  RCP_SNO: 1열, CKG_NM: 3열
+            recipe =  CsvUtil.findColumnValue(inputStream, 2, food, 0);
+
             if(recipe == null) {
                 System.out.println("Recipe not found");
                 return null;
             }
+
             return recipe;
         } catch (Exception e) {
             e.printStackTrace();

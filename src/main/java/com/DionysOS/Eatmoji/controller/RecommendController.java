eabaaef1b46f450773b0cc739586d1dbe1b6f920
jinha0907 <kjhakjha0101@gmail.com>
2025-06-06 16:06:42 +0900

    feat: 필요한 package import #15

--- src/main/java/com/DionysOS/Eatmoji/controller/RecommendController.java
+++ src/main/java/com/DionysOS/Eatmoji/controller/RecommendController.java
@@ -2,10 +2,17 @@ package com.DionysOS.Eatmoji.controller;
 
 import com.DionysOS.Eatmoji.dto.EmotionRequest;
 import com.DionysOS.Eatmoji.dto.RecommendResponse;
-import com.DionysOS.Eatmoji.dto.UserProfileRequest;
+import com.DionysOS.Eatmoji.model.History;
+import com.DionysOS.Eatmoji.model.User;
+import com.DionysOS.Eatmoji.repository.HistoryRepository;
 import com.DionysOS.Eatmoji.service.GptRecommendation;
 import org.springframework.web.bind.annotation.*;
+import com.DionysOS.Eatmoji.service.UserService;
+import com.DionysOS.Eatmoji.repository.UserRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/recommend")

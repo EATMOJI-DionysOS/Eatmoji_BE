bb11a11ad46578024645fa93c9e8db78791a0405
jinha0907 <kjhakjha0101@gmail.com>
2025-06-13 01:21:43 +0900

    fix: 바뀐 dto로 인한 자료형 수정 #80

--- src/main/java/com/DionysOS/Eatmoji/controller/RecommendController.java
+++ src/main/java/com/DionysOS/Eatmoji/controller/RecommendController.java
@@ -38,7 +38,7 @@ public class RecommendController {
         return response;
     }
     @PostMapping("/emoji/login")
-    public RecommendResponse loginRecommend(@RequestBody EmotionRequest request) {
+    public RecommendResponseWithID loginRecommend(@RequestBody EmotionRequest request) {
         String emoji = request.getEmoji();
 
         // 🟢 로그인한 사용자 이메일
@@ -65,7 +65,7 @@ public class RecommendController {
     }
 
     @PostMapping("/personalized")
-    public RecommendResponse recommendPersonalized() {
+    public RecommendResponseWithID recommendPersonalized() {
         String email = userService.getCurrentUserEmail();
 
         List<History> likedHistories = historyRepository.findLikedFoodsByEmail(email);
@@ -80,7 +80,7 @@ public class RecommendController {
         List<String> flavors = profile.get().getFlavor();
         List<String> diseases = profile.get().getDisease();
         List<String> allergies = profile.get().getAllergy();
-        RecommendResponse response = recommendationService.getAndSavePersonalizedRecommendation(
+        RecommendResponseWithID response = recommendationService.getAndSavePersonalizedRecommendation(
                 email, categories, flavors, diseases, allergies, likedFoods
         );
         return response;
